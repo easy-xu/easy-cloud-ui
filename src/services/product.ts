e44632d0598d4471af16576694422f65b13e8f48
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-09-03 23:32:59 +0800

    tomcat

--- src/services/product.ts
+++ src/services/product.ts
@@ -2,10 +2,10 @@ import { request } from 'umi';
 
 
 export function queryProductList(params: any) {
-    return request('/api/product/list', {
-        method: 'POST',
-        body: params,
-    });
+    // return request('/api/product/list', {
+    //     method: 'POST',
+    //     body: params,
+    // });
 }
 
 export function removeProducts(id: string) {
