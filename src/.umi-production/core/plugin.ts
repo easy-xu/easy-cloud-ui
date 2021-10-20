a9c95fb544c619ca2689b5cf5f4df645ab730d78
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-10-20 15:58:10 +0800

    删除不需要提交

--- src/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'C:/Project/simple-cloud-react/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
